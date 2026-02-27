7b306a4d1d4309a58b9b4415d13195cd04179cb7
rahatullah-dev <rahatullah.dev@gmail.com>
2026-02-27 10:04:53 +0500

    Updated Project 3

--- 03- Currency Converter/script.js
+++ 03- Currency Converter/script.js
@@ -11,7 +11,7 @@ const exchangeRateEl = document.getElementById("exchange-rate");
 updateRate()
 
 function updateRate(){
-    fetch(`https://v6.exchangerate-api.com/v6/5f9d1c87f7250195c9c9b17d/latest/${currencyFirstEl.value}`).then((res)=> res.json()).then((data)=>{
+    fetch(`https://v6.exchangerate-api.com/v6/26cf94b06bb1d3d93615b453/latest/${currencyFirstEl.value}`).then((res)=> res.json()).then((data)=>{
         const rate = data.conversion_rates[currencySecondEl.value];
         exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`;
 
@@ -24,3 +24,37 @@ currencyFirstEl.addEventListener("change", updateRate);
 currencySecondEl.addEventListener("change", updateRate);
 
 worthFirstEl.addEventListener("input",updateRate)
+
+exchangeRateEl.innerText = "Loading...";
+
+
+
+// auto text animation 
+
+const containerEl = document.getElementById("slogan");
+
+const careers = [
+    "I am Rahat Ullah",
+    "A Front-End",
+  "Web Developer",
+  "A Freelancer",
+  "& Graphic Designer",
+];
+
+let careerIndex = 0;
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `<h4>${careers[careerIndex].slice(0, 1) === "I" ? "" : ""} ${careers[careerIndex].slice(0, characterIndex)} </h4>`;
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 200);
+}
